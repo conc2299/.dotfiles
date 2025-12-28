732a382ea41b2d183d4de76e3aa336835c91f8f2
Yikai Liu <liuyk2023@shanghaitech.edu.cn>
2025-12-28 21:25:45 +0800

    source statement is directly appended into the corresponding .rc files

--- shell.sh
+++ shell.sh
@@ -5,12 +5,15 @@
 
 get_shell_sourcefile(){
     local shell_name="$1"
-    echo "$HOME/.config/$shell_name/sourcefile"
+    # echo "$HOME/.config/$shell_name/sourcefile"
+    echo "$HOME/.${shell_name}rc"
 }
 
 add_source(){
     local shell_name="$1"
     local to_source="$2"
     local source_file=$(get_shell_sourcefile "$shell_name")
-    append_if_missing "$source_file" "source $to_source"
+    if [ -f "$source_file" ]; then
+        append_if_missing "$source_file" "source $to_source"
+    fi
 }
